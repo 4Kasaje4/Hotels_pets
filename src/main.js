2de08a1042d4197b0202cdbc33ba7bb02b9cdd56
AaRrGguSs <sk.sumitta16@gmail.com>
2024-01-25 16:38:51 +0700

    loginnn

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue'
 import router from './router'
 import NavView from './components/Nav.vue/'
 import FooterView from './components/Footer.vue/'
+import newnavView from './components/newnav.vue'
 
 const app = createApp(App)
 
@@ -12,6 +13,7 @@ app.use(router)
 
 app.component('NavView',NavView)
 app.component('FooterView',FooterView)
+app.component('newnavView',newnavView)
 
 
 app.mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import about from '../views/about.vue'
 import Rejister from '../views/Rejister.vue'
 import packageView from '../views/package.vue'
 import login from '../views/login.vue'
+import mainpage from '../views/mainpage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,8 +33,12 @@ const router = createRouter({
       path:'/login',
       name:'login',
       component:login
+    },
+    {
+      path:'/mainpage',
+      name:'mainpage',
+      component:mainpage
     }
-    
    
    
   
