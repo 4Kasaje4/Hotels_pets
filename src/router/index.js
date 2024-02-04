9169cd18d7265549daedb0017964786945b8fced
AaRrGguSs <sk.sumitta16@gmail.com>
2024-02-04 16:18:00 +0700

    ส่งงานการจองบริการตัดขน

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import about from '../views/about.vue'
-import RegisterView from '@/views/Register.vue'
-import login from '../views/login.vue'
 import Homepage from '../views/Homepage.vue'
 import packageView from '@/views/package.vue'
 import caretakerView from '@/views/caretaker.vue'
-import ResetpasswordView from '@/views/Resetpassword.vue'
 import searchproView from '../views/searchpro.vue'
-import profileView from '@/views/profile.vue'
 import startpageView from '../views/start.vue'
 import newconpageView from '@/views/newconpage.vue'
+import cutconView from '@/views/cutcon.vue'
 
 
 
@@ -28,20 +25,9 @@ const router = createRouter({
       component:about
     },
     {
- 
-      path:'/Register',
-      name:'Register',
-      component:RegisterView
-    },
-    {
       path:'/package',
       name:'package',
       component:packageView
-    },
-    {
-      path:'/login',
-      name:'login',
-      component:login
     },
     {
       path:'/Homepage/:role/:id/:login_id',
@@ -54,19 +40,9 @@ const router = createRouter({
       component:caretakerView
     },
     {
-      path:'/Resetpassword/:role/:id/:login_id',
-      name:'resetpassword',
-      component:ResetpasswordView
-    },
-    {
       path:'/searchpro',
       name:'searchpro',
       component:searchproView
-    },
-    {
-      path:'/profile/:role/:id/:login_id',
-      name:'profile',
-      component:profileView
     },
     {
       path:'/start',
@@ -77,6 +53,11 @@ const router = createRouter({
       path:'/newconpage',
       name:'newconpage',
       component: newconpageView
+    },
+    {
+      path:'/cutcon',
+      name:'cutcon',
+      component: cutconView
     }
    
 
