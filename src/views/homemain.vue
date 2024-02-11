29730163231037949235128af1d6857228abf71c
AaRrGguSs <sk.sumitta16@gmail.com>
2024-02-11 16:52:36 +0700

    Create homemain.vue

---
+++ src/views/homemain.vue
@@ -0,0 +1,290 @@
+<template>
+    <NavView /> 
+    <div>
+  
+      <div class="container"> 
+        <div class="w3-content w3-section" style="max-width:500px">
+        <img class="mySlides" src="../img/หมาหมวน่ารัก.jpg"  width="1200px" height="650px">
+        <img class="mySlides" src="../img/หมาาาาาาาาาาาาา.jpg" width="1200px" height="650px">
+        <img class="mySlides" src="../img/cutecatdog.jpg"  width="1200px"  height="650px">
+  </div>
+      </div>
+  
+      <div class="centertext"> 
+           <img class="imgcenter" src="../img/logo.png" alt="">
+      </div>
+  
+      <div class="textcenter">
+        <h2 class="text1">ให้บริการดูแลและรับฝากสัตว์เลี้ยงของคุณ</h2>
+        <h2 class="text1">มีพี่เลี้ยงให้คำปรึกษาและดูแลด้วยความใส่ใจการเดินทางสะดวกสบาย</h2>
+        <h2 class="text1">มีห้องหลายขนาด รองรับน้องหมาสายพันธุ์ขนาดใหญ่</h2>
+        <h2 class="text1">และยังเข้ามาดูสัตว์เลี้ยงได้ตลอด 24 ชม.</h2>
+      </div>
+  
+      <div>
+        <h1 class="text2" >รายละเอียดห้องพักสัตว์เลี้ยง</h1>
+      </div>
+  
+        <div class="Bigbox">
+          <div class="smallbox">
+            <h1 class="textheadsm">ห้องพักสำหรับสุนัขขนาดเล็ก</h1>
+            <h2 class="texttent" > รายละเอียด </h2>
+            <p class="p1"> - ห้องพักขนาด 70 * 70</p>
+            <p class="p2"> - หมอนรองพื้น</p>
+            <p class="p3"> - ของเล่น</p>
+            <div class="pricebox"> <p class="pricetext">440 บาท/วัน</p></div>  
+              <img class="imgbig" src="../img/คอกเล็ก.webp" alt="" width="436px" height="357px">
+  
+              <div class="picbox">
+                <img class="imgsmall" src="../img/คอก.jpg" alt="" width="212px;" height="139px;" >
+                <img class="imgsmalll" src="../img/คอก3.jpg" alt="" width="212px;" height="139px;">
+              </div>
+          </div>  
+        </div>
+  
+        <div class="Bigbox">
+          <div class="smallbox">
+            <h1 class="textheadsm">ห้องพักสำหรับสุนัขขนาดเล็ก</h1>
+            <h2 class="texttent" > รายละเอียด </h2>
+            <p class="p1"> - ห้องพักขนาด 70 * 70</p>
+            <p class="p2"> - หมอนรองพื้น</p>
+            <p class="p3"> - ของเล่น</p>
+            <div class="pricebox"> <p class="pricetext">440 บาท/วัน</p></div>  
+              <img class="imgbig" src="../img/คอกเล็ก.webp" alt="" width="436px" height="357px">
+  
+              <div class="picbox">
+                <img class="imgsmall" src="../img/คอก.jpg" alt="" width="212px;" height="139px;" >
+                <img class="imgsmalll" src="../img/คอก3.jpg" alt="" width="212px;" height="139px;">
+              </div>
+          </div>  
+        </div>
+  
+        <div class="Bigbox">
+          <div class="smallbox">
+            <h1 class="textheadsm">ห้องพักสำหรับสุนัขขนาดทั่วไป</h1>
+            <h2 class="texttent" > รายละเอียด </h2>
+            <p class="p1"> - ห้องพักขนาด 200 * 150</p>
+            <p class="p2"> - หมอนรองพื้น</p>
+            <p class="p3"> - ของเล่น</p>
+            <div class="pricebox"> <p class="pricetext">700 บาท/วัน</p></div>  
+              <img class="imgbig" src="../img/บ้านหมาติดแอร์.jpeg" alt="" width="436px" height="357px">
+  
+              <div class="picbox">
+                <img class="imgsmall" src="../img/หลายห้อง.jpg" alt="" width="212px;" height="139px;" >
+                <img class="imgsmalll" src="../img/R.jpg" alt="" width="212px;" height="139px;">
+              </div>
+          </div>  
+        </div>
+  
+  
+        <div class="Bigbox">
+          <div class="smallbox">
+            <h1 class="textheadsm">ห้องพักสำหรับเเมวขนาดทั่วไป</h1>
+            <h2 class="texttent" > รายละเอียด </h2>
+            <p class="p1"> - ห้องพักขนาด 250 * 250</p>
+            <p class="p2"> - หมอนรองพื้น</p>
+            <p class="p3"> - ของเล่น</p>
+            <div class="pricebox"> <p class="pricetext">550 บาท/วัน</p></div>  
+              <img class="imgbig" src="../img/บ้านแมว.jpg" alt="" width="436px" height="357px">
+  
+              <div class="picbox">
+                <img class="imgsmall" src="../img/บ้าน2.webp" alt="" width="212px;" height="139px;" >
+                <img class="imgsmalll" src="../img/บ้าน3.jpg" alt="" width="212px;" height="139px;">
+              </div>
+          </div>  
+        </div>
+  
+                <h1 class="acti">กิจกรรมทั่วไปสำหรับสัตว์เลี้ยง</h1>
+                <div class="actibox">
+                  <img class="imgbox1" src="../img/หมาเดินเล่น.jpeg" alt="" width="350px" height="250px">
+                  <img class="imgbox2" src="../img/แมวเดิน.jpg" alt="" width="350px" height="250px">
+                  <img class="imgbox3" src="../img/หมาว่ายน้ำ.jpg" alt="" width="350px" height="250px">
+                </div>
+                  <div class="namebox">
+                    <h2 class="name1">เดินเล่น</h2>
+                    <h2 class="name2">เดินเล่น</h2>
+                    <h2 class="name3">เดินเล่น</h2>
+                  </div>
+  
+  
+      </div>
+  
+  
+  
+  
+  
+      <FooterView />
+  </template>
+  
+  
+  <script>
+  var myIndex = 0;
+  carousel();
+  
+  function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 5000); 
+  }
+  </script>
+  
+  <style scoped >
+  
+  
+  .container{
+   margin-left: 180px;
+   margin-top: 20px;
+  }
+  
+  .centertext{
+    display: flex;
+    justify-content: center;
+  }
+  .text1{
+    display: flex;
+    justify-content: center;
+    color: #838383;
+  }
+  .textcenter{
+    margin-top: -25px;
+  }
+  .text2{
+    display: flex;
+    justify-content: center;
+    color: #DA8F00;
+    margin-top: 90px;
+  }
+  
+  .Bigbox{
+    display: flex;
+    justify-content: center;
+    margin-top: 50px;
+  }
+  .smallbox{
+    width: 1108px;
+    height: 454px;
+    border-radius: 10px;
+    background: #F6F6F6;
+  }
+  .pricebox{
+    width: 141px;
+    height: 44px;border-radius: 25px;
+    background: linear-gradient(180deg, #FFF 0%, rgba(255, 255, 255, 0.72) 100%);
+    margin-left: 580px;
+    margin-top: 90px;
+    position: absolute;
+  }
+  .imgbig{
+    margin-left: -10px;
+    border-radius: 25px;
+    margin-top: 50px;
+    margin-left: 650px;
+  }
+  .picbox{
+    display: flex;
+  
+  }
+  .imgsmall{
+    margin-left: 140px;
+    margin-top: -140px;
+    border-radius: 25px;
+  }
+  .imgsmalll{
+    margin-left: 30px;
+    margin-top: -140px;
+    border-radius: 25px;
+  }
+  .pricetext{
+    margin-top: 12px;
+    margin-left: 30px;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  
+  }
+  .textheadsm{
+    margin-top: 40px;
+    margin-left: 130px;
+    position: absolute;
+  }
+  .texttent{
+    position: absolute;
+    margin-top: 90px;
+    margin-left: 195px;
+  }
+  .p1{
+    position: absolute;
+    margin-top: 130px;
+    margin-left: 195px;
+    font-weight: bold;
+    color: #838383;
+    font-size: 20px;
+  }
+  .p2{
+    position: absolute;
+    margin-top: 160px;
+    margin-left: 195px;
+    font-weight: bold;
+    color: #838383;
+    font-size: 20px;
+  }
+  .p3{
+    position: absolute;
+    margin-top: 190px;
+    margin-left: 195px;
+    font-weight: bold;
+    color: #838383;
+    font-size: 20px;
+  }
+  
+  .acti{
+    text-align: center;
+    margin-top: 30px;
+  }
+  
+  .imgbox1{
+    margin-top: 30px;
+    margin-left: 210px;
+    border-radius: 20px;
+  }
+  
+  .imgbox2{
+    margin-top: 30px;
+    margin-left: 30px;
+    border-radius: 20px;
+  }
+  
+  .imgbox3{
+    margin-top: 30px;
+    margin-left: 30px;
+    border-radius: 20px;
+  }
+  .namebox{
+    display: flex;
+  }
+  .name1{
+    margin-top: 10px;
+    margin-left: 350px;
+    color: #838383;
+  }
+  .name2{
+    margin-top: 10px;
+    margin-left: 300px;
+    color: #838383;
+  }
+  .name3{
+    margin-top: 10px;
+    margin-left: 300px;
+    color: #838383;
+  }
+  
+  
+  
+  
+  
+  
+  </style >
